54d5f0fa6b2f5cb4176a5ff6cfc1e80b7434a162
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-04-08 13:07:12 -0300

    exercicios

--- aulas/aula13/ambiente.js
+++ aulas/aula13/ambiente.js
@@ -1,12 +1,13 @@
 var cont = 1
 
-
+// enquanto (condição) {comando}
 while (cont <= 21) {
     // console.log('Tudo bem?')
     console.log(`Passo ${cont}`)
     cont++
 }
 
+// faca {comando} enquanto (condição)
 /*
  do {
     // console.log('Tudo bem?')
