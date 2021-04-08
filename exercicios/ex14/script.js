6d3fd0796a9277fb5a8e8ef09d738823396beb96
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-04-07 22:02:35 -0300

    exercicios

--- exercicios/ex14/script.js
+++ exercicios/ex14/script.js
@@ -1,2 +1,25 @@
-var img = window.getElementById('msg')
-var foto = window.getElementById('foto')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    
+    if (hora >=0 && hora < 12) {
+        // Bom dia
+        msg.innerHTML += `Bom dia!`
+        img.src = './images/manha.png'
+        document.body.style.background = '#4dabef'
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa tarde
+        msg.innerHTML += `Boa tarde!`
+        img.src = './images/tarde.png'
+        document.body.style.background = '#feb504'
+    } else {
+        // Boa noite
+        msg.innerHTML += `Boa noite!`
+        img.src = './images/noite.png'
+        document.body.style.background = '#2f414f'
+    }
+    msg.innerHTML += ` Agora são <strong>${hora}</strong> horas.`
+}
