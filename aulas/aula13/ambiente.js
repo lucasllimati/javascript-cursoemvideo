408e927d4989ffa777317ca0391479276b3feccb
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-04-08 13:04:20 -0300

    exercicios

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,26 @@
+var cont = 1
+
+
+while (cont <= 21) {
+    // console.log('Tudo bem?')
+    console.log(`Passo ${cont}`)
+    cont++
+}
+
+/*
+ do {
+    // console.log('Tudo bem?')
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 21)
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
